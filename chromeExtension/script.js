52d89ba2f690154007c4902cb430dc448c4ba9b6
Apurba Paul <48132427+apurbapaul1998@users.noreply.github.com>
2021-09-29 16:43:53 +0530

    Add files via upload

---
+++ chromeExtension/script.js
@@ -0,0 +1,26 @@
+//only manifest.jso with 3 properties will make you an extension
+//then we can add properties/features for what we want
+//here we are fetching jokes from "https://icanhazdadjoke.com/slack"
+//here everything is in an object
+//we need to fetch the jokes from the text
+//if we validate the json we are getting from the above website using "jsonlint.com", we will get
+// {
+// 	"attachments": [{
+// 		"fallback": "Why do you never see elephants hiding in trees? Because they're so good at it.",
+// 		"footer": "<https://icanhazdadjoke.com/j/t4MJRuXDIe|permalink> - <https://icanhazdadjoke.com|icanhazdadjoke.com>",
+// 		"text": "Why do you never see elephants hiding in trees? Because they're so good at it."
+// 	}],
+// 	"response_type": "in_channel",
+// 	"username": "icanhazdadjoke"
+// }
+
+
+fetch('https://icanhazdadjoke.com/slack')
+.then(data=>data.json())
+.then(jokeData=>{
+    const jokeText=jokeData.attachments[0].text; 
+    const jokeElement=document.getElementById('jokeElement');
+
+    jokeElement.innerHTML=jokeText;
+})
+
